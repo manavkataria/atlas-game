2fa6979ab9268f35c2aa0ae4657b19e0eaf08840
Manav Kataria <mk.newsgrp@gmail.com>
2012-09-24 18:44:50 -0500

    Adding Accept and Reject buttons using jQueryUI
    TODO: Add callbacks and hooks into firebase.

--- chat.js
+++ chat.js
@@ -51,36 +51,56 @@
   });
   
   var count = 0;
+  var setid = 1;  //buttonset id
   places = {};
-  
+
   // Add a callback that is triggered for each chat message.
   messagesRef.on('child_added', function (snapshot) {
     var message = snapshot.val();
 	  
-    var placehtml = [];
-    placehtml[0] = "";
+    var placeNode = [];
+    var buttonNode = [""];
 
+    placeNode[0] = "";
+    buttonNode[0] = "";
+    
   	count = count + 1;
   	
   	if (message.place != "") {
       mentionedBy = places[message.place];
-
+      
       if (mentionedBy) {
         $('#statusDiv').text(message.place + ' already mentioned by ' + mentionedBy);
-        placehtml = $('<span id="place-invalid"/>').html(message.place);
+        placeNode = $('<span class="place-invalid"/>').html(message.place);
       } else {
         places[message.place.toLowerCase()] = message.name.toLowerCase();
 
         //TODO: Make First and Last letter bold.
-        placehtml = $('<span id="place-valid"/>').html(message.place);
+        placeNode = $('<span class="place-valid"/>').html(message.place);
+
+        //create button set
+        buttonNode = $('<span id="radioA'+setid+'"></span>').html(
+          '<input type="radio" class="acceptbtn" id="radioA1'+setid+'" name="radioA'+setid+'" /><label for="radioA1'+setid+'">Accept</label>' +
+          '<input type="radio" class="rejectbtn" id="radioA2'+setid+'" name="radioA'+setid+'" /><label for="radioA2'+setid+'">Reject</label>').buttonset();
+        setid++;
+
       }
 
     } else {
       $('#statusDiv').text('Empty Place provided by ' + message.name);
     }
 
-    
   	//Attach html to 
-    $('<div/>').text(message.text + ' ').prepend($('<em/>').text(count + ": " + message.name + ': ')).append(placehtml[0]).appendTo($('#messagesDiv'));
+    $('<div/>').text(message.text + ' ').prepend($('<em/>').text(count + ": " + message.name + ': ')).append(placeNode[0]).append(buttonNode[0]).appendTo($('#messagesDiv'));
     $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+
+    $( ".acceptbtn").button({
+      icons: { primary: "ui-icon-check" }, 
+      text: false
+    }); 
+    $( ".rejectbtn").button({
+      icons: { primary: "ui-icon-closethick" }, 
+      text: false
+    });
+
   });
