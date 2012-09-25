86bcf1a3eac2bcb58bf0288b3415cbf3b20140f9
Manav Kataria <mk.newsgrp@gmail.com>
2012-09-24 20:52:16 -0500

    Right align accept and reject icons

--- chat.js
+++ chat.js
@@ -1,10 +1,9 @@
-/* Global Variables */
+  /* Global Variables */
   var name = "";
+  var count = 0;
+  var setid = 1;  //buttonset id
+  places = {};
   
-  /* 
-  input = "Hello! Atlassians! How you doing? :) /Alaska Nebraska /Canada"
-  return = "Alaska"
-  */
   function getPlace(text) {
     var str = "";
     
@@ -50,9 +49,6 @@
     }
   });
   
-  var count = 0;
-  var setid = 1;  //buttonset id
-  places = {};
 
   // Add a callback that is triggered for each chat message.
   messagesRef.on('child_added', function (snapshot) {
@@ -79,28 +75,27 @@
         placeNode = $('<span class="place-valid"/>').html(message.place);
 
         //create button set
-        buttonNode = $('<span id="radioA'+setid+'"></span>').html(
-          '<input type="radio" class="acceptbtn" id="radioA1'+setid+'" name="radioA'+setid+'" /><label for="radioA1'+setid+'">Accept</label>' +
-          '<input type="radio" class="rejectbtn" id="radioA2'+setid+'" name="radioA'+setid+'" /><label for="radioA2'+setid+'">Reject</label>').buttonset();
+        buttonNode = $('<span class="button-set" id="buttonSet'+setid+'"></span>').html(
+          '<input type="radio" class="accept-btn" id="acceptBtn'+setid+'" name="buttonSet'+setid+'" /><label for="acceptBtn'+setid+'">Accept</label>' +
+          '<input type="radio" class="reject-btn" id="rejectBtn'+setid+'" name="buttonSet'+setid+'" /><label for="rejectBtn'+setid+'">Reject</label>').buttonset();
         setid++;
-
       }
 
     } else {
-      $('#statusDiv').text('Empty Place provided by ' + message.name);
+      $('#statusDiv').text('Empty place provided by ' + message.name);
     }
 
   	//Attach html to 
     $('<div/>').text(message.text + ' ').prepend($('<em/>').text(count + ": " + message.name + ': ')).append(placeNode[0]).append(buttonNode[0]).appendTo($('#messagesDiv'));
     $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
 
-    $( ".acceptbtn").button({
+    $( ".accept-btn").button({
       icons: { primary: "ui-icon-check" }, 
       text: false
     }); 
-    $( ".rejectbtn").button({
+    $( ".reject-btn").button({
       icons: { primary: "ui-icon-closethick" }, 
       text: false
     });
 
-  });
+  }); //Child Added Callback
